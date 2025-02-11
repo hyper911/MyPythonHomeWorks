bd673ee72e43ec9604cc0e6f3da3c45d89223131
hyper911 <55325055+hyper911@users.noreply.github.com>
2025-02-11 10:13:36 +0200

    HW 09.12.2024

--- HW_OOP_09.12.2024.py
+++ HW_OOP_09.12.2024.py
@@ -11,6 +11,13 @@ class Student:
         self.gladness = 50
         self.progress = 0
         self.alive = True
+        self.money = 100
+
+    def to_work(self):
+        print("I'm working")
+        self.progress += -0.01
+        self.gladness -= 1
+        self.money += 100
 
     def to_study(self):
         print("I'm study")
@@ -25,32 +32,37 @@ class Student:
         print("Rest time")
         self.progress -= 0.1
         self.gladness += 5
+        self.money -= 50
 
     def is_alive(self):
         if self.progress < -0.5:
-            print("Відрахували")
+            print("Відрахували!")
             self.alive = False
         elif self.gladness <= 0:
-            print("Йому потрібний друг")
+            print("Йому потрібний друг!")
             self.alive = False
         elif self.progress > 5:
             print("Закінчив курс екстерном")
             self.alive = False
+            print()
 
     def end_of_day(self):
         print(f"Gladness - {self.gladness}")
         print(f"Progress - {round(self.progress, 2)}")
+        print(f"Money - {self.money}")
 
     def live(self, day):
         print(f"Day {day} of {self.name}")
 
-        live_cube = random.randint(1, 3)
+        live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
+        elif live_cube == 4:
+            self.to_work()
 
         self.end_of_day()
         self.is_alive()
